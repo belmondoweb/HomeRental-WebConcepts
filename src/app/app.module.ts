3ece1f05a2ba714ab5d6da9c883ab6215a8baed9
Natalie Peretz <belmondoweb@gmail.com>
2019-05-13 16:00:32 +0300

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgbModule
+    HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
